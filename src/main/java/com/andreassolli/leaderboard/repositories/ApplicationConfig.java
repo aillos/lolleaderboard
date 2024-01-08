fc02768acd9a75e395f4386ef758f5d31fff1b17
aillos <91605277+aillos@users.noreply.github.com>
2024-01-08 15:02:45 +0100

    Changed application.properties

---
+++ src/main/java/com/andreassolli/leaderboard/repositories/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package com.andreassolli.leaderboard.repositories;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Value("DATABASEURL")
+    private String databaseUrl;
+
+    @Bean
+    public DataSource dataSource(){
+        return DataSourceBuilder
+                .create()
+                .url(databaseUrl)
+                .build();
+    }
+}

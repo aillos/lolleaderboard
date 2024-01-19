be0cccc5e6307289fef8108aa5445ea4bb60e35a
aillos <91605277+aillos@users.noreply.github.com>
2024-01-19 18:33:00 +0100

    Attempt at fixing security

--- src/main/java/com/andreassolli/leaderboard/config/SecurityConfig.java
+++ src/main/java/com/andreassolli/leaderboard/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET,
-                                "/index*", "/static/**", "/*.js", "/*.json", "/*.ico", "/")
+                                "/index*", "/static/**", "/*.js", "/*.json", "/*.ico", "/", "/assets/**")
                         .permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/**").permitAll()

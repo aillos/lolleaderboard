722b002cb9076e143e8ba13740457c6d968f2d6c
aillos <91605277+aillos@users.noreply.github.com>
2024-01-19 17:57:36 +0100

    Attempt at fixing security

--- src/main/java/com/andreassolli/leaderboard/config/SecurityConfig.java
+++ src/main/java/com/andreassolli/leaderboard/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.andreassolli.leaderboard.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -40,6 +41,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/**", "").permitAll()
                         .requestMatchers("/auth/welcome", "/auth/addNewUser", "/auth/generateToken").permitAll()
                         .requestMatchers("/auth/user/**").authenticated()
